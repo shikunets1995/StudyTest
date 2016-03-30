d516c54e98b3dd72c4bb2a47289e26a4b667a2d9
eshikunets <shikunets1995@gmail.com>
2016-03-30 13:37:22 +0300

    ads

--- RecyclerApp/src/main/java/com/netcosports/studytest/MainActivity.java
+++ RecyclerApp/src/main/java/com/netcosports/studytest/MainActivity.java
@@ -2,12 +2,44 @@ package com.netcosports.studytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        RecyclerAdapter adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+        adapter.setData(getData());
     }
-}
+
+
+
+    private List<String> getData() {
+        List<String> data = new ArrayList<>();
+        for (int i = 0; i < 99; ++i) {
+            data.add(" " +
+                    "" + i);
+        }
+        return data;
+    }
+    };
+
